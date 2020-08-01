d7c2dee890a8eec84f221946dc22b6137200742c
mo3tafa <mostafa.hosseini69@gmail.com>
2020-08-01 06:31:07 -0700

    step 2

--- Djapp/urls.py
+++ Djapp/urls.py
@@ -1,8 +1,12 @@
 from django.urls import path,include
 from django.conf.urls import url
+from . import views
 
-app_name = 'Djapp'
-
+app_name = 'Djapp';
 urlpatterns = [
-    # path('')
+    # path('', views.index,name = 'index'),
+    # url(r'^$', views.index, name = 'index'),
+    url(r'^$', views.Home.as_view(), name = 'index'),
+    url(r'^books/$', views.BookListView.as_view(), name='books'),
+    url(r'^book/(?P<pk>\d+)$', views.BookDetailView.as_view(), name='book-detail'),
 ]
--- Djapp/views.py
+++ Djapp/views.py
@@ -1,3 +1,40 @@
+from django.views import generic
 from django.shortcuts import render
+from . import models
 
-# Create your views here.
+class Home(generic.TemplateView):
+    template_name = 'Djapp/index.html'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['num_books'] = models.Book.objects.all().count()
+        context['num_instances'] = models.BookInstance.objects.all().count()
+        context['num_instances_available'] = models.BookInstance.objects.filter(status__exact='a').count()
+        context['num_authors'] = models.Author.objects.count()
+        return context
+# def index(request):
+#     """
+#     View function for home page of site.
+#     """
+#     # Generate counts of some of the main objects
+#     num_books=models.Book.objects.all().count()
+#     num_instances=models.BookInstance.objects.all().count()
+#     # Available books (status = 'a')
+#     num_instances_available=models.BookInstance.objects.filter(status__exact='a').count()
+#     num_authors=models.Author.objects.count()  # The 'all()' is implied by default.
+#
+#     # Render the HTML template index.html with the data in the context variable
+#     return render(
+#         request,
+#         'Djapp/index.html',
+#         context={'num_books':num_books,'num_instances':num_instances,'num_instances_available':num_instances_available,'num_authors':num_authors},
+#     )
+class BookListView(generic.ListView):
+    model = models.Book
+    context_object_name = 'book_list'
+    template_name = 'Djapp/book_list.html'
+class BookDetailView(generic.TemplateView):
+    model = models.Book
+    context_object_name = 'book'
+    template_name = 'Djapp/book_detail.html'
+    
